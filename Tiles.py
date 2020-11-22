15509be65244408776bb1f88ff415f62e57da145
Artur <arturs13-12@inbox.lv>
2020-11-22 23:08:44 +0200

    Add files via upload

--- Tiles.py
+++ Tiles.py
@@ -3,23 +3,44 @@
 """
 
 import os, gdal, sys
- 
-in_path = '/home/arturs/Data/RGB/'
-input_filename = sys.argv[1]
- 
-out_path = sys.argv[2]
-os.mkdir(out_path)
-output_filename = sys.argv[3] + '_tile_'
- 
-tile_size_x = 224
-tile_size_y = 224
- 
+import itertools
+import multiprocessing
+
+in_path = '/home/arturs/Data/RGB1/'
+input_filename = '4114-14_1.tif'
+os.mkdir('/home/arturs/Data/RGB_Tiles1/')
+
 ds = gdal.Open(in_path + input_filename)
 band = ds.GetRasterBand(1)
 xsize = band.XSize
 ysize = band.YSize
- 
-for i in range(0, xsize, tile_size_x):
-    for j in range(0, ysize, tile_size_y):
-        com_string = "gdal_translate -a_srs EPSG:3059 -a_nodata 0.0 -of JPEG -srcwin " + str(i)+ ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + str(in_path) + str(input_filename) + " " + str(out_path) + str(output_filename) + str(i) + "_" + str(j) + ".tif"
-        os.system(com_string)
+
+tile_size_x = 224
+tile_size_y = 224
+
+#Indices i and j
+i = range(0, xsize, tile_size_x)
+j = range(0, ysize, tile_size_y)
+
+#The list contain all possible combinations of parameters.
+paramlist = list(itertools.product(i,j))
+
+#A function which will process a tuple of parameters
+def translate(params):
+  i = params[0]
+  j = params[1]
+  com_string = "gdal_translate -a_srs EPSG:3059 -a_nodata 0.0 -srcwin " + str(i)+ ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + str(in_path) + str(input_filename) + " " + str(out_path) + str(output_filename) + str(i) + "_" + str(j) + ".tif"
+  os.system(com_string)
+  return None
+
+filelist = [file for file in os.listdir('/home/arturs/Data/RGB1/') if file.endswith('.tif')]
+for file in filelist:
+    input_filename = file
+    out_path = '/home/arturs/Data/RGB_Tiles1/' + os.path.splitext(file)[0] + '/' #'/home/arturs/Data/CIR_Tiles/'
+    os.mkdir(out_path)
+    output_filename = os.path.splitext(file)[0]
+    #Generate processes equal to the number of cores
+    pool = multiprocessing.Pool()
+
+    #Distribute the parameter sets evenly across the cores
+    res  = pool.map(translate,paramlist)
